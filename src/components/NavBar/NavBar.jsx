ad84043db0704870588d1875872c7b4e1599ab29
SeanELLee <seankitwahlee@gmail.com>
2024-01-04 19:38:45 -0800

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'Portfolio-React/main'.
    
    Changes to be committed:
    	modified:   dist/index.html
    	modified:   src/components/NavBar/NavBar.jsx

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -11,7 +11,7 @@ function NavBar(){
     <div>
       <nav id="nav_bar">
         <ul id = "menu_nav">
-          <li><a className='elements_nav' href = "/portfolio-react">Home</a></li>
+          <li><a className='elements_nav' href = "/portfolio-react/">Home</a></li>
           <li><a className='elements_nav' href = "/portfolio-react/About">About</a></li>
           <li><a className='elements_nav' href = "/portfolio-react/Portfolio">Portfolio</a></li>
           <li><a className='elements_nav' href = "/portfolio-react/Contact">Contact</a></li>
